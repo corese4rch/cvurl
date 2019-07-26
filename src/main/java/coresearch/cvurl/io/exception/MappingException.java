40c4f011edbbed0ef30474c4bf432b9c3b94b92b
yorekKap <yorek.kap@gmail.com>
2019-07-26 12:36:54 +0300

    Remove redundant constructor from MappingException

--- src/main/java/coresearch/cvurl/io/exception/MappingException.java
+++ src/main/java/coresearch/cvurl/io/exception/MappingException.java
@@ -7,18 +7,6 @@ package coresearch.cvurl.io.exception;
 public class MappingException extends RuntimeException {
 
     /**
-     * Constructs a new exception with the specified detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
-    public MappingException(String message) {
-        super(message);
-    }
-
-    /**
      * Constructs a new exception with the specified detail message and
      * cause.  <p>Note that the detail message associated with
      * {@code cause} is <i>not</i> automatically incorporated in
@@ -31,7 +19,6 @@ public class MappingException extends RuntimeException {
      *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      */
-
     public MappingException(String message, Throwable cause) {
         super(message, cause);
     }
