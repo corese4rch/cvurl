6fce0a3e97b43ba8e1f1a42545853603d3c6d982
yorekKap <yorek.kap@gmail.com>
2019-10-10 16:00:19 +0300

    Add comments to CVType.

--- src/main/java/coresearch/cvurl/io/mapper/CVType.java
+++ src/main/java/coresearch/cvurl/io/mapper/CVType.java
@@ -3,6 +3,13 @@ package coresearch.cvurl.io.mapper;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+/**
+ * Class that describes type with generics. Used as parameter to some of {@link coresearch.cvurl.io.request.Request}
+ * methods to provide ability to parse response to body to class with generics.
+ * Usage example: <br/>
+ * {@code cvurl.get(url).asObject(new CVType<List<Users>() {});} <br/>
+ * this code snippet would parse response body to {@code List<User>}
+ */
 public abstract class CVType<T> {
     private Type type;
 
@@ -16,6 +23,9 @@ public abstract class CVType<T> {
         this.type = type.getActualTypeArguments()[0];
     }
 
+    /**
+     * @return actual type
+     */
     public Type getType() {
         return type;
     }
